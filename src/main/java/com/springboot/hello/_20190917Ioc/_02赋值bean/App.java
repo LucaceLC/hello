c46f8511f49f4a264bbdbf8822ce32391b50cad0
shanyuefengcheng <848202342@gmail.com>
2019-09-17 16:10:11 +0800

    ioc属性赋值

---
+++ src/main/java/com/springboot/hello/_20190917Ioc/_02赋值bean/App.java
@@ -0,0 +1,34 @@
+package com.springboot.hello._20190917Ioc._02赋值bean;
+
+import com.springboot.hello._20190917Ioc._00Constant.SpringConstants;
+import com.springboot.hello._20190917Ioc._01注册bean.PersonService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.stream.Stream;
+
+@Slf4j
+public class App {
+    @Test
+    public void fucIoc(){
+        /**
+         * 从路径下加载xml，应用上下文
+         * 初始化spring容器
+         * 获取id的实体bean，强转
+         * 执行bean的方法
+         * 关闭容器
+         */
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(SpringConstants.Ioc.XML_PATH);
+
+        String[] beanNames = context.getBeanDefinitionNames();
+        Stream.of(beanNames).forEach(System.out::println);
+
+        try {
+            Car car = (Car)context.getBean("car");
+            System.out.println(car);
+        }finally {
+            context.close();
+        }
+    }
+}
