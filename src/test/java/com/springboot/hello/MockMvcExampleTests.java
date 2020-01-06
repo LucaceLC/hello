dd6511a17e5dd864c64c1ae0fce4c944fafd2490
shanyuefengcheng <848202342@gmail.com>
2020-01-06 14:02:16 +0800

    一种常量方法

--- src/test/java/com/springboot/hello/MockMvcExampleTests.java
+++ src/test/java/com/springboot/hello/MockMvcExampleTests.java
@@ -1,23 +1,34 @@
 package com.springboot.hello;
 
-import com.springboot.hello._20191105MybatisPlus联系._1105CURD基础.service.OrderService;
-import org.junit.Test;
+import com.springboot.hello._20200104一种配置变量初始化的方法.UrlConstants;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+//由于是Web项目，Junit需要模拟ServletContext，因此我们需要给我们的测试类加上@WebAppConfiguration。
+@WebAppConfiguration
 public class MockMvcExampleTests {
 
-    @Autowired
-    private OrderService orderService;
 
-	@Test
-	public void exampleTest() throws Exception {
-		//System.out.println();
+	@Before
+	public void init() {
+		System.out.println("开始测试-----------------");
 	}
 
+	@After
+	public void after() {
+		System.out.println("测试结束-----------------");
+	}
+
+	@Test
+	public void valueFunction(){
+		System.out.println(UrlConstants.RPC_PAY_URL);
+	}
 }
