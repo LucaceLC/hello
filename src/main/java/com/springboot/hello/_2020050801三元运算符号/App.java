4739db5298372b6a2f253ff84f5db7775835d5fd
shanyuefengcheng <848202342@gmail.com>
2020-05-08 13:55:30 +0800

    三元运算符号

---
+++ src/main/java/com/springboot/hello/_2020050801三元运算符号/App.java
@@ -0,0 +1,10 @@
+package com.springboot.hello._2020050801三元运算符号;
+
+public class App {
+
+    public static void main(String[] args) {
+        int a = -1;
+        String result = a >= 0? "正数"  :"负数";
+        System.out.println(result);
+    }
+}
