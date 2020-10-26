0d60d724814050c5c34d614b84730f4b5c13275d
shanyuefengcheng <848202342@gmail.com>
2020-10-26 17:51:20 +0800

    如何判断链表有环

--- src/main/java/com/springboot/hello/_2020050705字符串连接符/App.java
+++ src/main/java/com/springboot/hello/_2020050705字符串连接符/App.java
@@ -9,7 +9,7 @@ public class App {
     public void strOperation() {
         /**
          * 两个知识点
-         *  + 二元操作符，只要有一个是字符串，重载为字符串连接符
+         *  + 一元操作符，只要有一个是字符串，重载为字符串连接符
          *  + 的顺序是从左到右
          */
         String a = "5";
@@ -17,5 +17,7 @@ public class App {
         int c = 5;
 
         Assertions.assertEquals("95",b+c+a);
+
+
     }
 }
---
+++ src/main/java/com/springboot/leetcode/链表/_2020102601链表判断是否有环/Solution.java
@@ -0,0 +1,79 @@
+package com.springboot.leetcode.链表._2020102601链表判断是否有环;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 单链列表的定义
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+
+    //hash
+    public boolean hasCycleWithHash(ListNode head){
+        Set<ListNode> listNodeSet = new HashSet<>();
+        while (head != null){
+            if(!listNodeSet.add(head)){
+               return true;
+            }
+            else {
+                head = head.next;
+            }
+        }
+        return false;
+    }
+
+    //do while
+    public boolean hasCycleWithFastSlow(ListNode head) {
+        if (head == null || head.next == null){
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        do {
+            //判断前面的兔子有没有走到头，走到头，跳出循环
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            //自身移动
+            slow = slow.next;
+            fast = fast.next.next;
+        } while (fast != slow);
+
+        return true;
+    }
+
+    //while
+    public boolean hasCycleMethod(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){
+        this.val = x;
+        next = null;
+    }
+}
