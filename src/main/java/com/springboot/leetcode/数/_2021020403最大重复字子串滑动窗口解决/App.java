62929672fd20f93b19b88f06282f72624481e1d6
shanyuefengcheng <848202342@gmail.com>
2021-02-04 21:28:25 +0800

    最大子串的滑动窗口问题

--- src/main/java/com/springboot/leetcode/数/_2021020403最大重复字子串滑动窗口解决/App.java
+++ src/main/java/com/springboot/leetcode/数/_2021020403最大重复字子串滑动窗口解决/App.java
@@ -1,5 +1,6 @@
 package com.springboot.leetcode.数._2021020403最大重复字子串滑动窗口解决;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,4 +36,30 @@ public class App {
         }
         return max;
     }
+
+    public int findMaxStr2(String str){
+        if(str == null || str.isEmpty()) return 0;
+        char[] chars = str.toCharArray();
+        //a-z :97-122   A-Z :65 -90
+        int[] lastOccurrence =  new int[128];
+        Arrays.fill(lastOccurrence,-1);
+
+        int fast =0, slow = 0,max = 0;
+        for (; fast < chars.length; fast++){
+            char element = chars[fast];
+            if(lastOccurrence[element] > -1){
+                int currentSlow = lastOccurrence[element] +1;
+                if(slow < currentSlow){
+                    slow = currentSlow;
+                }
+            }
+            lastOccurrence[element] = fast;
+            int currentMax = fast -slow +1;
+            if(max < currentMax){
+                max = currentMax;
+            }
+        }
+        return max;
+    }
+
 }
