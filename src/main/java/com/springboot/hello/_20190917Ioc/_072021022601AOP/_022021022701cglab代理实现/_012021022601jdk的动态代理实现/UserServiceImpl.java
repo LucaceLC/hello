1105d1f98408aa68b899a5dafc8b1915ac70e881
luocheng <848202342@qq.com>
2021-02-28 00:19:39 +0800

    cglib字节码增强，只有实现类，原理是在代码运行期间，创建目标类的子类对象，进行增强 1增强列enhancer 2设置目标类为父类 3设置回调方法，MethodInterceptor方法拦截器，参数1 代理对象，参数二 当前执行的方法，参数三传参，参数4方法的代理 4创建代理对象，object需要强转

---
+++ src/main/java/com/springboot/hello/_20190917Ioc/_072021022601AOP/_022021022701cglab代理实现/_012021022601jdk的动态代理实现/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.springboot.hello._20190917Ioc._072021022601AOP._022021022701cglab代理实现._012021022601jdk的动态代理实现;
+
+public class UserServiceImpl{
+    public void addUser() {
+        System.out.println("add user");
+    }
+
+    public void updateUser() {
+        System.out.println("add user");
+    }
+}
