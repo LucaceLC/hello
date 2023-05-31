521c9ac753812975362d995d837e671283da3800
shanyuefengcheng <848202342@gmail.com>
2023-05-31 22:39:46 +0800

    mq的消息防止丢失kafka

---
+++ src/main/java/com/springboot/hello/_2023053001MQ/_2023053101MQ的顺序性/APP.java
@@ -0,0 +1,11 @@
+package com.springboot.hello._2023053001MQ._2023053101MQ的顺序性;
+
+public class APP {
+    /**
+     *
+     * rabbit mq
+     * 保证有序的消息，都进入一个mq的队列
+     *
+     * 确保一个消费者只从一个topic中消费
+     */
+}
