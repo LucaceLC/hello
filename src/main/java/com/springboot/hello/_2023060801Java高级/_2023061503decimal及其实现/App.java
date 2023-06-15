f2c3dab2cfde79919263052129006cccd502e239
shanyuefengcheng <848202342@gmail.com>
2023-06-15 16:57:37 +0800

    垃圾回收算法

---
+++ src/main/java/com/springboot/hello/_2023060801Java高级/_2023061503decimal及其实现/App.java
@@ -0,0 +1,15 @@
+package com.springboot.hello._2023060801Java高级._2023061503decimal及其实现;
+
+public class App {
+    /**
+     * decimal及其实现
+     *
+     * de色谋的实现
+     *
+     * decimal底层使用bigInteger标识整数部分，一个int标识小数点后位数，一个表示精度
+     *
+     * bigInteger底层实现是一个标识-1负数，1正数，0就是0
+     * 还维护一个int数组 mag
+     * 如果整数大于32位，那么作为第一ge数组元素存放，以此类推
+     */
+}
