19356123d8d43946943ec43a10c94fa225cb5fea
shanyuefengcheng <848202342@gmail.com>
2023-06-15 15:34:43 +0800

    百万数据插入

--- src/main/java/com/springboot/hello/_2023060501互联网高并发设计题/_2023061302如何保证幂等性性/App.java
+++ src/main/java/com/springboot/hello/_2023060501互联网高并发设计题/_2023061302如何保证幂等性性/App.java
@@ -1,4 +1,10 @@
 package com.springboot.hello._2023060501互联网高并发设计题._2023061302如何保证幂等性性;
 
 public class App {
+    /**
+     * 数据库唯一键
+     * 业务标识唯一键
+     * 乐观锁-版本号
+     * 悲观锁-for update
+     */
 }
