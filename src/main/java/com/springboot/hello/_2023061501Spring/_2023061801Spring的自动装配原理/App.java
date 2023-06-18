d1a8acc554b41f7d34311d2d615e74960b182672
shanyuefengcheng <848202342@qq.com>
2023-06-18 22:13:24 +0800

    自动化配置原理流程

--- src/main/java/com/springboot/hello/_2023061501Spring/_2023061801Spring的自动装配原理/App.java
+++ src/main/java/com/springboot/hello/_2023061501Spring/_2023061801Spring的自动装配原理/App.java
@@ -2,6 +2,13 @@ package com.springboot.hello._2023061501Spring._2023061801Spring的自动装配�
 
 public class App {
     /**
-     *
+     * Springboot主配置类
+     * 开启自动配置注解
+     * import导入自动配置类的选择器
+     * 扫描所有meta-info目录下的spring.factories文件
+     * 把所有的自动化配置类导入到容器中
+     * 自动化配置类解析到所以的对应配置属性类
+     * 根据限定条件生效配置类，对应的注入bean组件时，使用对应的属性类属性初始化bean
+     * 完成bean注
      */
 }
